945198b53146a01c818871d7666da13c81f62b51
ardmn9 <pkmngolbat@hotmail.com>
2018-04-29 21:02:45 +0000

    Added routes

--- Aquidemia/config/routes.rb
+++ Aquidemia/config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  
+  # index page / root url
+  root 'pages#home', as: 'home'
+  
+  get 'pages/home'
+  
+  get 'pages/search'
+
+  get 'pages/favorites'
+
+  get 'pages/about'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
