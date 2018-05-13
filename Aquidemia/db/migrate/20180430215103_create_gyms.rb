030e15bac872a66683bfc94ed603c38d654396d8
tos96 <tos@cin.ufpe.br>
2018-05-13 20:09:54 +0000

    Atualização das migrations e seeds.rb, adição do arquivo Gyms.csv

--- Aquidemia/db/migrate/20180430215103_create_gyms.rb
+++ Aquidemia/db/migrate/20180430215103_create_gyms.rb
@@ -1,7 +1,6 @@
 class CreateGyms < ActiveRecord::Migration[5.1]
   def change
     create_table :gyms do |t|
-      t.integer :number
       t.integer :rpa
       t.string :microregion
       t.integer :cnes
--- Aquidemia/db/seeds.rb
+++ Aquidemia/db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'csv'
+
+csv_text = File.read(Rails.root.join('lib', 'opendata', 'Gyms.csv'))
+csv = CSV.parse(csv_text, :headers => true)
+i = 0
+csv.each do |row|
+  g = Gym.new
+  i = i+1
+  g.rpa = row['rpa']
+  g['microregion'] = row['micro_regiao']
+  g['cnes'] = row['cnes']
+  g['name'] = row['academia_nome']
+  g['adress'] = row['endereco']
+  g['bairro'] = row['bairro']
+  g['phone'] = row['fone']
+  g['latitude'] = row['latitude']
+  g['longitude'] = row['longitude']
+  g['hour'] = row['horario_de_aulas']
+  g['rating'] = 7
+  g.save
+end
