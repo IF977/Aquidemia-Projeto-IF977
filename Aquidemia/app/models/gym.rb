8b5cf06e65b11339247be519a6c9c43c3519aa2b
thomastbb <thomas_tbb@hotmail.com>
2018-06-09 20:00:27 +0000

    Adding markers and marker window

--- Aquidemia/app/models/gym.rb
+++ Aquidemia/app/models/gym.rb
@@ -1,9 +1,4 @@
 class Gym < ApplicationRecord
     acts_as_voteable
     acts_as_followable
-    #geocoded_by :address, :latitude  => :latitude, :longitude => :longitude
-    #after_validation :geocode
-    #def address
-    #    [adress, bairro].compact.join(', ')
-    #end    
 end
